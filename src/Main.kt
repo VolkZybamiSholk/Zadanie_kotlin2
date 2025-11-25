d1f010c5c914e73565d238ea293f95514d416ebf
AndreyMelnikov1993 <145114868+AndreyMelnikov1993@users.noreply.github.com>
2025-11-25 15:29:44 +0300

    котлин2

--- src/Main.kt
+++ src/Main.kt
@@ -17,7 +17,7 @@ fun agoToText(secondsA: Int): String {
     return when (secondsA) {
         in 0..60 -> "Был(а) только что"
         in 61 until (60 * 60) -> minutesAgo((secondsA / 60))
-        in (60 * 60) until (24 * 60 * 60) -> hoursAgo((secondsA / (60 * 60))) // Теперь правильно работает
+        in (60 * 60) until (24 * 60 * 60) -> hoursAgo((secondsA / (60 * 60)))
         in (24 * 60 * 60) until (24 * 60 * 60 * 2) -> "Был(а) вчера"
         in (24 * 60 * 60 * 2) until (24 * 60 * 60 * 3) -> "Был(а) позавчера"
         else -> "Был(а) давно"
@@ -54,15 +54,15 @@ fun zadanie2() {
     when(Card.lowercase()) {
         "vkpay" -> {
             //Если вк плей то проверяем соотвествующие лимиты, если не превышены, то без коммиссии перевод или ошибка
-            if(MonthAmount <= 40000 && SummAmount <= 15000) println("Реквизит ${Card} итоговая сумма перевода ${SummAmount}")
+            if(MonthAmount <= 40000 && SummAmount <= 15000 && SummAmount > 0) println("Реквизит ${Card} итоговая сумма перевода ${SummAmount}")
             else println("Ошибка перевода по ${Card} сумма перевода ${SummAmount}")
         }
         //Если мастеркард или маестро то проверяем соотвествующие лимиты, если не превышены,
         // то проверяем чтобы перевод был до 75000 рублей тогда без коммиссии
         //или будет с коммисией
         "mastercard", "maestro" -> {
-            if(MonthAmount <= 600000 && SummAmount <= 150000) {
-                if(SummAmount < 75000) println("Реквизит ${Card} итоговая сумма перевода ${SummAmount}")
+            if(MonthAmount <= 600000 && SummAmount <= 150000 && SummAmount > 0) {
+                if(SummAmount >= 300 && SummAmount < 75000) println("Реквизит ${Card} итоговая сумма перевода ${SummAmount}")
                 else println("Реквизит ${Card} итоговая сумма перевода ${SummAmount-((SummAmount*0.006)+20)}")
             } else println("Ошибка перевода по ${Card} сумма перевода ${SummAmount}")
 
@@ -70,9 +70,8 @@ fun zadanie2() {
         //если visa или мир то проверяем лимиты и минимум перевод 35 рублей
         //тогда считаем сразу коммисию, так без коммиссии нету перевода в этом варианте карт
         "visa", "мир" -> {
-            if(MonthAmount <= 600000 && SummAmount <= 150000) {
-                if ((SummAmount*0.0075) < 36) println("Реквизит ${Card} итоговая сумма перевода ${SummAmount-35}")
-                else println("Реквизит ${Card} итоговая сумма перевода ${SummAmount-(SummAmount*0.0075)}")
+            if(MonthAmount <= 600000 && SummAmount <= 150000 && SummAmount > 0) {
+                if ((SummAmount >= 35) println("Реквизит ${Card} итоговая сумма перевода ${SummAmount-(SummAmount*0.0075)}")
             } else println("Ошибка перевода по ${Card} сумма перевода ${SummAmount}")
         }
         else -> println("Ошибка! Неправильно написан реквизит перевода")
